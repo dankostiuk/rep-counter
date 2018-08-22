de90a8345ca168f1cc57303addb72910bfc3c1cf
Dan Kostiuk <dank216@gmail.com>
2018-08-21 23:08:08 -0400

    Added image to entry screen, improved each exercise form of currentDay

--- src/components/WorkoutPicker.js
+++ src/components/WorkoutPicker.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import image from '../css/images/dumbell.png';
 
 class WorkoutPicker extends React.Component {
 
@@ -17,19 +18,21 @@ class WorkoutPicker extends React.Component {
 
 	render() {
 		return (
-		<form className="workout-selector" onSubmit={this.goToWorkout}>
-			<h2>Select workout</h2>
-			<select name="workout" ref={this.workoutRef}>
-			  <option value="chest">Chest</option>
-			  <option value="back">Back</option>
-			  <option value="shoulders">Shoulders</option>
-			  <option value="arms">Arms</option>
-			  <option value="legs">Legs</option>
-			  <option value="cardio">Cardio</option>
-			</select>
-			<button type="submit">Begin Workout</button>
-		</form>
-		)
+      <form className="workout-selector" onSubmit={this.goToWorkout}>
+        <img src={image}/>
+        <h2>RepCounter</h2>
+        <h5>Select Workout:</h5>
+  			<select name="workout" ref={this.workoutRef}>
+  			  <option value="chest">Chest</option>
+  			  <option value="back">Back</option>
+  			  <option value="shoulders">Shoulders</option>
+  			  <option value="arms">Arms</option>
+  			  <option value="legs">Legs</option>
+  			  <option value="cardio">Cardio</option>
+  			</select>
+  			<button type="submit">Begin Workout</button>
+      </form>
+	   )
 	}
 }
 
